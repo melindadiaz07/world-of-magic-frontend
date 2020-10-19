6117df07c615c791781ed9ba860465bca9bb1004
aysikh <amy.sikhammountry@gmail.com>
2020-10-19 16:03:32 -0400

    created form and users render to houses

--- src/index.js
+++ src/index.js
@@ -5,6 +5,9 @@ const housesURL = "http://localhost:3000/houses"
 
 document.addEventListener("DOMContentLoaded", () => {
   getHouses()
+  document.querySelector(".new-submit-button").addEventListener('click', () => {
+    newUserForm(event)
+  });
 })
 
 function getHouses(){
@@ -30,11 +33,38 @@ function renderHouse(house){
   pointsEl.innerText = `Points: ${house.points}`
   houseDiv.append(pointsEl)
 
+  let houseUserUl = document.createElement('ul')
+  houseUserUl.id = `ul-${house.id}-house`
+  houseDiv.append(houseUserUl)
 
-
+  house.users.forEach(singleUser => {
+    renderUserList(singleUser, house)
+  });
 }
 
+function renderUserList(user, house){
+  let userUl = document.querySelector(`#ul-${house.id}-house`)
+  // console.log(userUl)
+  let userLi = document.createElement("li")
+  userUl.append(userLi)
+  userLi.innerText = user.name
+}
 
-
-
-
+function newUserForm(event){
+  event.preventDefault();
+  let newUserName = document.querySelector(".new-user-name").value
+  let newHouse = Math.floor(Math.random()*4 +1)
+  
+  fetch(usersURL, {
+    method: "POST",
+    headers: {"Content-Type": "application/json"},
+    body: JSON.stringify({name: newUserName, house_id: newHouse})
+  })
+  .then(res => res.json())
+  .then(newUser => {
+    let houseUl = document.querySelector(`#ul-${newHouse}-house`)
+    let newUserLi = document.createElement("li")
+    newUserLi.innerText = newUser.name
+    houseUl.append(newUserLi)
+  });
+}
