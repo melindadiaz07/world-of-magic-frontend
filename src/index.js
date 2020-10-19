c825bf5453b3632b15d96c5d5aac623311baa2fd
Melindadiaz07 <melindadiaz@me.com>
2020-10-19 12:57:42 -0400

    added first javascript rendering framework

--- src/index.js
+++ src/index.js
@@ -1,10 +1,38 @@
 const creaturesURL = "http://localhost:3000/creatures"
 const usersURL = "http://localhost:3000/users"
+const housesURL = "http://localhost:3000/houses"
 
 
 document.addEventListener("DOMContentLoaded", () => {
   getHouses()
 })
+
+function getHouses(){
+  fetch(housesURL)
+  .then(res => res.json())
+  .then(houseData => houseData.forEach(house => renderHouse(house)))
+}
+
+function renderHouse(house){
+  
+  let headerDiv = document.querySelector('#header')
+
+  let houseDiv = document.createElement('div')
+  headerDiv.append(houseDiv)
+
+  houseDiv.id = `${house.name}-header`
+  let houseTitleEl = document.createElement('h2')
+  houseTitleEl.textContent = house.name
+  houseDiv.append(houseTitleEl)
+  houseDiv.id = `${house.name}-title`
+
+  let pointsEl = document.createElement('p')
+  pointsEl.innerText = `Points: ${house.points}`
+  houseDiv.append(pointsEl)
+
+
+
+}
 
 
 
