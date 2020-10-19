0b2de5bf20773d5ae38f00d617ccfd70d07dd3f8
Melindadiaz07 <melindadiaz@me.com>
2020-10-19 12:17:00 -0400

    first commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+const creaturesURL = "http://localhost:3000/creatures"
+const usersURL = "http://localhost:3000/users"
+
+
+document.addEventListener("DOMContentLoaded", () => {
+  getHouses()
+})
+
+
+
+
+
